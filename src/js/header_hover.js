d1a715d3adf8ee0d54b258fbd61f683afaaac654
kristina.lobanets <kristina.malitowska@gmail.com>
2020-05-05 22:31:27 +0300

    section_1

--- src/js/header_hover.js
+++ src/js/header_hover.js
@@ -1,35 +1,10 @@
-'use strict';
 
-var burgerbtn = document.querySelector('.header__burder'),
-    closebtn = document.querySelector('.'),
-    burgertoggle = document.querySelector('.bd-modal-burger'),
-    headermenu = document.querySelector('.header__top'),
-    btnlink = document.querySelector('.header__btn--tell'),
-    nav = document.querySelector('.header__nav'),
-    display = document.querySelector('.display'),
-    header__link = document.querySelector('.header__link'),
+let el = document.getElementById('btn');
 
+el.addEventListener('click', e => {
+    e.preventDefault();
 
-    burgerbtn.onclick = function () {
-        burgertoggle.style.display = 'block';
-        headermenu.style.visibility = 'hidden';
-    };
+    let menu = document.getElementById('menu');
+    menu.classList.toggle('opened');
+});
 
-closebtn.onclick = function () {
-    burgertoggle.style.display = 'none';
-    headermenu.style.visibility = 'visible';
-};
-
-btnlink.onclick = function () {
-    nav.style.display = 'none';
-    btnlink.style.display = 'none';
-    display.style.display = 'block';
-};
-
-header__link.onclick = function () {
-    burgertoggle.style.display = 'block';
-    headermenu.style.visibility = 'hidden';
-    nav.style.display = 'none';
-    display.style.display = 'block';
-    btnlink.style.display = 'none';
-};
