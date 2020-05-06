0ce98255ec2f1ba0894fce2917c87614071215eb
kristina.lobanets <kristina.malitowska@gmail.com>
2020-05-06 16:13:34 +0300

    i am winner! I am win js section_1

--- src/js/header_hover.js
+++ src/js/header_hover.js
@@ -1,7 +1,13 @@
 let el = document.getElementById("btn");
+let ele = document.getElementById("btn_t");
 let closeButton = document.getElementById("close");
 let menu = document.getElementById("menu");
 el.addEventListener("click", (e) => {
+    e.preventDefault();
+    menu.classList.add("opened");
+
+});
+ele.addEventListener("click", (e) => {
     e.preventDefault();
     menu.classList.add("opened");
 
